7efbb928ee0fbc785eb2e98fffab4bf771ad3535
Schnup89 <Tobias_Tsafi@gmx.de>
2021-10-21 22:32:47 +0200

    travis test

--- main.js
+++ main.js
@@ -3,7 +3,6 @@
 /*
  * Created with @iobroker/create-adapter v1.23.0
  */
-
 // The adapter-core module gives you access to the core ioBroker functions
 // you need to create an adapter
 const utils = require("@iobroker/adapter-core");
